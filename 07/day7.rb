a8151a2d78f040b8b8798abef38ce2a3311d5ecc
Niklas Nilsson <niklasnson@me.com>
2022-12-07 21:14:19 +0100

    Day7

---
+++ 07/day7.rb
@@ -0,0 +1,27 @@
+lines = File.read("input.txt").split("\n").freeze
+
+stack = []
+dirs = Hash.new(0)
+
+lines.each do |line|
+  case line
+  when /\$ cd (.+)/
+    dir = "#{$1}"
+    dir == ".." ? stack.pop : stack << dir
+  when /(\d+) (.+)/
+    path = stack.dup
+    loop do
+      dirs[path.dup] += $1.to_i
+      path.pop
+      break if path.empty?
+    end
+  end
+end
+
+puts  "Q1: #{ dirs.values.select { |size| size < 100_000 }.sum }"
+
+USED = dirs[["/"]]
+TARGET = USED - (70_000_000-30_000_000)
+
+puts "Q2: #{dirs.values.sort.detect { |size| size >= TARGET } }"
+
